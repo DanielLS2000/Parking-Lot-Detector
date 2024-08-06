d2f1610b6dc5155eb0527af42928a25746d1fdd8
Daniel <daniellimas2000@gmail.com>
2024-08-05 21:55:12 -0300

    final push hopefully

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 import cv2
 import numpy as np
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from PIL import Image, ImageTk
 
+def calculate_color_histogram(image):
+    histograms = []
+    for i in range(3):
+        hist = cv2.calcHist([image], [i], None, [256], [0, 256])
+        cv2.normalize(hist, hist)
+        histograms.append(hist)
+    return histograms
+
+def compare_histograms(hist1, hist2, method=cv2.HISTCMP_CORREL):
+    scores = []
+    for h1, h2 in zip(hist1, hist2):
+        score = cv2.compareHist(h1, h2, method)
+        scores.append(score)
+    return np.mean(scores)
+
 class Vaga:
-    def __init__(self, coords, ocupada):
+    def __init__(self, coords, ocupada, hist):
         self.coords = coords
+        self.ocupada_inicialmente = ocupada
         self.ocupada = ocupada
+        self.initial_hist = hist
+        self.occupation_history = [ocupada]
+
+    def register_occupation(self):
+        if len(self.occupation_history) < 30:
+            self.occupation_history.append(self.ocupada)
+        else:
+            self.occupation_history.pop(0)
+            self.occupation_history.append(self.ocupada)
 
 
 class VideoApp:
@@ -78,11 +103,8 @@ class VideoApp:
         scale_percent = 0.75  # x% da janela
         new_width = int(self.window_width * scale_percent)
         new_height = int(frame_height * (new_width / frame_width))
-        
-        # Redimensionar o frame
         frame_resized = cv2.resize(frame, (new_width, new_height), interpolation=cv2.INTER_AREA)
         
-        # Converter frame de BGR para RGB
         frame_rgb = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2RGB)
         img = Image.fromarray(frame_rgb)
         imgtk = ImageTk.PhotoImage(image=img)
@@ -142,14 +164,14 @@ class VideoApp:
 
     def processaFrame(self, frame):
         grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        thresh = cv2.adaptiveThreshold(grayFrame, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 25, 9)
-        imgMediana = cv2.medianBlur(thresh, 5)
-        kernel = np.ones((3,3), np.int8)
-        frame_processado = cv2.morphologyEx(imgMediana, cv2.MORPH_OPEN, kernel)
 
-        laplacian = cv2.Laplacian(frame, cv2.CV_64F)
+        # Aplicar filtro Gaussiano
+        imagem_suavizada = cv2.GaussianBlur(grayFrame, (5, 5), 0)
 
-        self.mostra_miniatura(laplacian)
+        # Aplicar o algoritmo de Canny
+        frame_processado = cv2.Canny(imagem_suavizada, 100, 200)
+
+        self.mostra_miniatura(frame_processado)
         return frame_processado
 
     def drawVagas(self, frame, frame_processado):
@@ -159,11 +181,24 @@ class VideoApp:
             relativeWhitePixels = whitePixels / self.calcArea(vaga.coords)
             cv2.putText(frame, str(round(relativeWhitePixels, 2)), (vaga.coords[0][0], vaga.coords[0][1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
             
+
+            recorte2 = self.recortar_Imagem(frame, vaga.coords)
+            current_hist = calculate_color_histogram(recorte2)
+            similarity = compare_histograms(vaga.initial_hist, current_hist)
+            
             if relativeWhitePixels >= 0.2:
                 vaga.ocupada = True
-                cv2.polylines(frame, [np.array(vaga.coords).reshape((-1, 1, 2))], isClosed=True, color=(0, 0, 255), thickness=2)
+            elif similarity >= 0.6:
+                vaga.ocupada = vaga.ocupada_inicialmente
             else:
                 vaga.ocupada = False
+            vaga.register_occupation()
+
+
+            ocupada = sum(1 for v in vaga.occupation_history if v) > len(vaga.occupation_history) / 2
+            if ocupada:
+                cv2.polylines(frame, [np.array(vaga.coords).reshape((-1, 1, 2))], isClosed=True, color=(0, 0, 255), thickness=2)
+            else:
                 cv2.polylines(frame, [np.array(vaga.coords).reshape((-1, 1, 2))], isClosed=True, color=(0, 255, 0), thickness=2)
         
         vagasOcupadas = sum(1 for vaga in self.vagas if vaga.ocupada)
@@ -186,17 +221,28 @@ class VideoApp:
 
     def click_event(self, event, x, y, flags, params):
         frame = self.first_frame.copy()
-        frame = self.drawVagas(frame, self.processaFrame(frame))
         if event == cv2.EVENT_LBUTTONDOWN:
+            frame = self.drawVagas(frame, self.processaFrame(frame))
             self.pontos.append((x, y))
             
             if len(self.pontos) == 4:
-                vaga = Vaga(self.pontos, False)
+                
+                vaga = Vaga(self.pontos, self.question_popup(), calculate_color_histogram(self.recortar_Imagem(self.first_frame, self.pontos)))
                 self.vagas.append(vaga)
                 self.pontos = []
                 frame = self.first_frame.copy()
                 frame = self.drawVagas(frame, self.processaFrame(frame))
             cv2.imshow('image', frame)
+
+    def question_popup(self):
+        root = tk.Tk()
+        root.withdraw()  # Oculta a janela principal
+
+        result = messagebox.askokcancel("Vaga está ocupada?", "ok = sim ; cancelar = não")
+
+        root.destroy()  # Fecha a janela principal
+
+        return result
 
     def recortar_Imagem(self, imagem, coords):
         pts1 = np.float32(coords)
